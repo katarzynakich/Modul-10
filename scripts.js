c46c1f3da5417fcbb74e5d70c801561c0aa009aa
katarzynakich <katarzynakich@gmail.com>
2017-06-11 23:48:47 +0200

    poprawiony

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@ var span = $("span");
 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+    $(element).append(button);
 });
 
 $("button").click(function(){
